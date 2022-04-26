add7f60828313171ffed1d32c30c5cc0865ef379
Max W <mwinter@wesleyan.edu>
2022-04-25 20:46:21 -0500

    started homepage

---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Homepage = () => {
+
+    return (
+        <section>
+            <div class="carousel" data-flickity='{ "wrapAround": true }'>
+                <div class="carousel-cell"></div>
+                <div class="carousel-cell"></div>
+                <div class="carousel-cell"></div>
+                <div class="carousel-cell"></div>
+                <div class="carousel-cell"></div>
+            </div>
+        </section>
+    );
+};
+
+export default Homepage;
